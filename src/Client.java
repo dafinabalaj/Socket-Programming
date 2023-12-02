3153255966554560acba5dabea68db647fdf88db
blineramehmeti1 <121365571+blineramehmeti1@users.noreply.github.com>
2023-12-02 18:07:01 +0100

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -69,7 +69,7 @@ public class Client {
 
 
         public static void main(String args[]) {
-            Client client = new Client("Server IP", 55000);
+            Client client = new Client("Server IP", 5000);
         }
     }
 
